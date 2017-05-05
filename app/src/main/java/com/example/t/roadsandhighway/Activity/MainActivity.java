cd8487fa99a4ad70c24574267bc5b54e11cdf1ab
MuhtasimTanmoy <taaanmo0oy@gmail.com>
2017-05-05 23:55:47 +0600

    Embedded sms functionality added + project Structured

--- app/src/main/java/com/example/t/roadsandhighway/Activity/MainActivity.java
+++ app/src/main/java/com/example/t/roadsandhighway/Activity/MainActivity.java
@@ -1,5 +1,6 @@
-package com.example.t.roadsandhighway;
+package com.example.t.roadsandhighway.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,9 +8,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.example.t.roadsandhighway.R;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.EventListener;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,6 +27,7 @@ public class MainActivity extends AppCompatActivity implements MeteorCallback {
 
     private Meteor mMeteor;
     private Button button;
+    private Button smsActivity;
     private static String TAG = "checkThisTag";
 
     @Override
@@ -30,10 +35,11 @@ public class MainActivity extends AppCompatActivity implements MeteorCallback {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = (Button) findViewById(R.id.test);
+        smsActivity= (Button) findViewById(R.id.sendsms);
 
 
         // create a new instance
-        mMeteor = new Meteor(this, "ws://192.168.0.101:3000/websocket");
+        mMeteor = new Meteor(this, "ws://192.168.0.109:3000/websocket");
 
         // register the callback that will handle events and receive messages
         mMeteor.addCallback(this);
@@ -45,52 +51,70 @@ public class MainActivity extends AppCompatActivity implements MeteorCallback {
             @Override
             public void onClick(View v) {
 
-                if (mMeteor.isConnected()) {
-                    mMeteor.loginWithUsername("bishwa", "1234", new ResultListener() {
-                        @Override
-                        public void onSuccess(String result) {
-                            Log.d(TAG, "Logged in: " + result);
 
-                            String subscriptionId = mMeteor.subscribe("Statuses");
-                            Toast.makeText(getApplicationContext(), Boolean.toString(mMeteor.isConnected()) + " and" + subscriptionId, Toast.LENGTH_LONG).show();
-                            Map<String, Object> values = new HashMap<String, Object>();
-                            values.put("level", "TJ-1");
-                            values.put("averageSpeed", "");
-                            values.put("trafficVolume", "");
-                            values.put("note", "");
-                            values.put("filePath", null);
-                            values.put("latitude", 22.4);
-                            values.put("longitude", 90);
-                            values.put("address", "BUET");
-                            Object[] queryParams = {values};
-                            mMeteor.call("statuses.insert",queryParams,new ResultListener() {
 
-                                @Override
-                                public void onSuccess(String result) {
-                                    Log.d(TAG, "success  in inserting" );
-
-                                }
-
-                                @Override
-                                public void onError(String error, String reason, String details) {
-                                    Log.d(TAG, "Error: " + error + " " + reason + " " + details);
-
-                                }
-                            });
-                        }
-
-                        @Override
-                        public void onError(String error, String reason, String details) {
-                            Log.d(TAG, "Error: " + error + " " + reason + " " + details);
-                        }
-                    });
-                }
+                meteorTestDataInsert();
 
 
             }
         });
 
 
+
+
+        smsActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,SmsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+
+    void meteorTestDataInsert(){
+        if (mMeteor.isConnected()) {
+            mMeteor.loginWithUsername("tanmoy", "tanmoy", new ResultListener() {
+                @Override
+                public void onSuccess(String result) {
+                    Log.d(TAG, "Logged in: " + result);
+
+                    String subscriptionId = mMeteor.subscribe("Statuses");
+                    Toast.makeText(getApplicationContext(), Boolean.toString(mMeteor.isConnected()) + " and" + subscriptionId, Toast.LENGTH_LONG).show();
+                    Map<String, Object> values = new HashMap<String, Object>();
+                    values.put("level", "TJ-2");
+                    values.put("averageSpeed", "");
+                    values.put("trafficVolume", "");
+                    values.put("note", "");
+                    values.put("filePath", null);
+                    values.put("latitude", 22.4);
+                    values.put("longitude", 90);
+                    values.put("address", "BUET");
+                    Object[] queryParams = {values};
+                    mMeteor.call("statuses.insert",queryParams,new ResultListener() {
+
+                        @Override
+                        public void onSuccess(String result) {
+                            Log.d(TAG, "success  in inserting" );
+
+                        }
+
+                        @Override
+                        public void onError(String error, String reason, String details) {
+                            Log.d(TAG, "Error: " + error + " " + reason + " " + details);
+
+                        }
+                    });
+                }
+
+                @Override
+                public void onError(String error, String reason, String details) {
+                    Log.d(TAG, "Error: " + error + " " + reason + " " + details);
+                }
+            });
+        }
     }
 
     @Override
---
+++ app/src/main/java/com/example/t/roadsandhighway/Activity/SmsActivity.java
@@ -0,0 +1,125 @@
+package com.example.t.roadsandhighway.Activity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.t.roadsandhighway.R;
+
+/**
+ * Created by t on 5/5/17.
+ */
+
+public class SmsActivity extends AppCompatActivity{
+    EditText number;
+    EditText content;
+    Button sendButton;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sms_activity);
+        number= (EditText) findViewById(R.id.number);
+        content= (EditText) findViewById(R.id.content);
+        sendButton= (Button)findViewById(R.id.smsButton);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mobileNumber=number.getText().toString();
+                String body=content.getText().toString();
+
+                //This one directly sends sms :)
+                performSMS(mobileNumber,body);
+            }
+        });
+
+
+
+    }
+
+    void performSMS(String number,String body){
+
+
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.SEND_SMS)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.SEND_SMS},
+                        1);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }else {
+            try {
+                SmsManager smsManager = SmsManager.getDefault();
+                smsManager.sendTextMessage(number, null,body, null, null);
+                Toast.makeText(this, "Message Sent",
+                        Toast.LENGTH_LONG).show();
+            } catch (Exception ex) {
+                Toast.makeText(this,ex.getMessage().toString(),
+                        Toast.LENGTH_LONG).show();
+                ex.printStackTrace();
+            }
+        }
+
+
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    performSMS(number.getText().toString(),content.getText().toString());
+
+                } else {
+
+                    Toast.makeText(this, "Not granted", Toast.LENGTH_SHORT).show();
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+}
