6df112e0dc97f3b11648898321e370196c3e55f4
MuhtasimTanmoy <taaanmo0oy@gmail.com>
2017-05-18 16:19:17 +0600

    Meteor subscription working fine

--- app/src/main/java/com/example/t/roadsandhighway/Activity/test.java
+++ app/src/main/java/com/example/t/roadsandhighway/Activity/test.java
@@ -1,18 +1,25 @@
 package com.example.t.roadsandhighway.Activity;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.t.roadsandhighway.R;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.io.IOException;
+import java.util.List;
 
 import im.delight.android.ddp.Meteor;
 import im.delight.android.ddp.MeteorCallback;
 import im.delight.android.ddp.ResultListener;
-import im.delight.android.ddp.db.Collection;
+import im.delight.android.ddp.SubscribeListener;
 import im.delight.android.ddp.db.Document;
 import im.delight.android.ddp.db.memory.InMemoryDatabase;
 
@@ -25,17 +32,21 @@ public class test extends AppCompatActivity implements MeteorCallback {
     private Meteor mMeteor;
     private Button button;
     private static String TAG = "checkThisTag";
+    EditText editText;
+    Button seeLatLang;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = (Button) findViewById(R.id.test);
+        seeLatLang= (Button) findViewById(R.id.seeLatLang);
+        editText= (EditText) findViewById(R.id.et);
 
 
 
         // create a new instance
-        mMeteor = new Meteor(this, "ws://192.168.1.103:3000/websocket",new InMemoryDatabase());
+        mMeteor = new Meteor(this, "ws://192.168.1.106:3000/websocket",new InMemoryDatabase());
 
         // register the callback that will handle events and receive messages
         mMeteor.addCallback(this);
@@ -43,66 +54,52 @@ public class test extends AppCompatActivity implements MeteorCallback {
         // establish the connection
         mMeteor.connect();
 
+
+
+
+
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
 
-                //                if (mMeteor.isConnected()) {
-//                    mMeteor.loginWithUsername("tanmoy", "tanmoy", new ResultListener() {
-//                        @Override
-//                        public void onSuccess(String result) {
-//                            //Log.d(TAG, "Logged in: " + result);
-//
-//                            String subscriptionId = mMeteor.subscribe("Statuses");
-//                            Toast.makeText(getApplicationContext(), Boolean.toString(mMeteor.isConnected()) + " and" + subscriptionId, Toast.LENGTH_LONG).show();
-
-
-
-
-                Log.d(TAG, "Logged in: " + mMeteor.isConnected());
-
+                Log.d(TAG, String.valueOf(mMeteor.isConnected()));
                 if (mMeteor.isConnected()) {
                     mMeteor.loginWithUsername("tanmoy", "tanmoy", new ResultListener() {
                         @Override
                         public void onSuccess(String result) {
-                            //Log.d(TAG, "Logged in: " + result);
-
-                            String subscriptionId = mMeteor.subscribe("Statuses");
-                            Toast.makeText(getApplicationContext(), Boolean.toString(mMeteor.isConnected()) + " and" + subscriptionId, Toast.LENGTH_LONG).show();
-
-                            mMeteor.call("testMethod",null,new ResultListener() {
+                            String[] params = {};
+                            String subscriptionId = mMeteor.subscribe("statuses", params, new SubscribeListener() {
 
                                 @Override
-                                public void onSuccess(String result) {
-                                    Log.d(TAG, "success  in inserting"+result);
+                                public void onSuccess() {
+                                   Log.d(TAG,"Yes");
+
+                                        Document[] documents = mMeteor.getDatabase().getCollection("Statuses").find();
+
+                                        for (Document x : documents) {
+                                            Log.d(TAG, (String) x.getField("level"));
+                                        }
 
                                 }
-
-                                ;
 
                                 @Override
                                 public void onError(String error, String reason, String details) {
-                                    Log.d(TAG,error+reason+details);
+                                    Toast.makeText(getApplicationContext(),"Error",Toast.LENGTH_SHORT).show();
+
                                 }
-
-                                ;
                             });
-//                            Document[] documents = mMeteor.getDatabase().getCollection("Statuses").find();
-//
-//                            Log.d(TAG, "Logged in: " + documents);
-//                            for (Document doc :documents){
-//
-//                                Log.d(TAG, "Logged in: " + doc);
-//
-//                            }
 
-//                            String[] collectionNames = mMeteor.getDatabase().getCollectionNames();
-//                            for (String doc :collectionNames){
-//
-//                                Log.d(TAG, "Logged in: " + doc);
-//
-//                            }
+
+
+
+                            String[] collectionNames = mMeteor.getDatabase().getCollectionNames();
+                            for (String doc :collectionNames){
+
+                                Log.d(TAG, "Logged in: " + doc);
+
+                            }
 //
 //                            Collection collection = mMeteor.getDatabase().getCollection("users");
 //                            Log.d(TAG, "Logged in: " + collection);
@@ -117,6 +114,18 @@ public class test extends AppCompatActivity implements MeteorCallback {
                         }
                     });
                 }
+
+
+            }
+        });
+
+
+
+
+
+        seeLatLang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
 
             }
@@ -142,14 +151,14 @@ public class test extends AppCompatActivity implements MeteorCallback {
     @Override
     public void onDataAdded(String collectionName, String documentID, String newValuesJson) {
 
-            Log.d(TAG,newValuesJson);
+            Log.d(TAG,"ON Data added>>>>>>>>"+newValuesJson+" ------------ "+collectionName);
 
 
     }
 
     @Override
     public void onDataChanged(String collectionName, String documentID, String updatedValuesJson, String removedValuesJson) {
-        Log.d(TAG,updatedValuesJson+removedValuesJson);
+        //Log.d(TAG,updatedValuesJson+removedValuesJson);
     }
 
     @Override
